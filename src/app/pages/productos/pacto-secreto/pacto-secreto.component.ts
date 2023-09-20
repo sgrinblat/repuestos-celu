9092c0dd3b280ccb4d94624008ec0ebdc02e2009
sgrinblat <sgrinblat@asjservicios.com>
2023-09-20 11:24:52 -0300

    pacto venta

--- src/app/pages/productos/pacto-secreto/pacto-secreto.component.ts
+++ src/app/pages/productos/pacto-secreto/pacto-secreto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GoogleAnalyticsService } from 'ngx-google-analytics';
 
 @Component({
   selector: 'app-pacto-secreto',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PactoSecretoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gaService: GoogleAnalyticsService) { }
 
   ngOnInit() {
   }
 
+  onButtonClick(buttonLabel: string) {
+    this.gaService.event('click', 'Buttons', buttonLabel);
+  }
 }
