a28f4b95d26768f375154efbbd2b0b9a74c8b28f
sgrinblat <sgrinblat@asjservicios.com>
2023-10-19 14:47:38 -0300

    arregle pedidos calculadora

--- src/app/pages/decklist/image-boveda-deck/image-boveda-deck.component.ts
+++ src/app/pages/decklist/image-boveda-deck/image-boveda-deck.component.ts
@@ -19,7 +19,7 @@ export class ImageBovedaDeckComponent {
   async generarImagen(decklist: string, nombreCompleto: string) {
     // Crear contenedor de imágenes
     const contenedor = document.createElement('div');
-    contenedor.style.width = '1150px';
+    contenedor.style.width = '1160px';
     contenedor.style.height = '600px';
     contenedor.style.backgroundImage = 'url("/assets/images/texture.png")';
     contenedor.style.backgroundSize = 'cover'; // La imagen cubre todo el contenedor
