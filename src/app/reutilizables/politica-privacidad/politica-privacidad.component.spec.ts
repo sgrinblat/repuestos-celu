dc700725b1845ee98a3195220f2540e28532e5a2
Santiago Grinblat <grinblatsantiago@gmail.com>
2023-11-05 18:26:53 -0300

    buscador finalmente fixeado

---
+++ src/app/reutilizables/politica-privacidad/politica-privacidad.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { PoliticaPrivacidadComponent } from './politica-privacidad.component';
+
+describe('PoliticaPrivacidadComponent', () => {
+  let component: PoliticaPrivacidadComponent;
+  let fixture: ComponentFixture<PoliticaPrivacidadComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PoliticaPrivacidadComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PoliticaPrivacidadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
