7bbb2de72ce5f1ca9a88e27b8e8a70f7175d1c09
Santiago Grinblat <grinblatsantiago@gmail.com>
2024-01-20 23:03:19 -0300

    catalogo

--- src/app/pages/tiendas/folleto/folleto.component.ts
+++ src/app/pages/tiendas/folleto/folleto.component.ts
@@ -58,6 +58,21 @@ export class FolletoComponent implements OnInit {
 
     // this.cotizacionService.getCotizaciones().subscribe(data => {
     //   this.cotizaciones = data;
+
+    //   this.cotizacionService.getCotizacionesCripto().subscribe(data => {
+    //     this.cotizacionCripto = data;
+    //     this.cotizacionCripto.totalAsk += 20;
+
+    //     if(this.cotizacionCripto.totalAsk < 1000 && this.cotizaciones.blue.value_sell < 1000) {
+    //       this.cotizacionMostrada = 1000;
+    //     } else if(this.cotizacionCripto.totalAsk > this.cotizaciones.blue.value_sell) {
+    //       this.cotizacionMostrada = this.cotizacionCripto.totalAsk;
+    //       this.cotizacionMostrada += 10;
+    //     } else {
+    //       this.cotizacionMostrada = this.cotizaciones.blue.value_sell;
+    //       this.cotizacionMostrada += 10;
+    //     }
+    //   });
     // });
 
     this.codigo = prompt("Ingrese el código para ingresar a esta sección: ");
@@ -100,6 +115,12 @@ export class FolletoComponent implements OnInit {
       precioDolar: 21,
       stock: true
     },
+    // {
+    //   nombre: 'Slot de Torneo tienda (2 cajas de boosters + cartas promos)',
+    //   imagenUrl: 'https://lairentcg.com.ar/assets/images/pacto/caja%202.png',
+    //   precioDolar: 42,
+    //   stock: true
+    // },
     {
       nombre: 'Relatos de Aixa',
       imagenUrl: 'https://lairentcg.com.ar/assets/images/frente%20mazo%20aixa.webp',
@@ -114,14 +135,14 @@ export class FolletoComponent implements OnInit {
     }
   ];
 
-  // mazodoble: Producto[] = [
-  //   {
-  //     nombre: 'Mazo Dual - 2 jugadores',
-  //     imagenUrl: 'https://i.postimg.cc/wMhwHR7k/frente-1.png',
-  //     precioDolar: 40,
-  //     stock: true
-  //   }
-  // ];
+  slotDeTorneo: Producto[] = [
+    {
+      nombre: 'Slot de Torneo tienda',
+      imagenUrl: 'https://lairentcg.com.ar/assets/images/pacto/slot.png',
+      precioDolar: (this.productos[1].precioDolar * 2) * 0.7,
+      stock: true
+    },
+  ];
 
   calcularTotalUnidades(): number {
     return this.productos.reduce((acc, producto) => acc + (producto.cantidad || 0), 0);
@@ -168,44 +189,44 @@ export class FolletoComponent implements OnInit {
 
 
 
-  // calcularTotalUnidadesFolleto(): number {
-  //   return this.mazodoble.reduce((acc, producto) => acc + (producto.cantidad || 0), 0);
-  // }
+  calcularTotalUnidadesFolleto(): number {
+    return this.slotDeTorneo.reduce((acc, producto) => acc + (producto.cantidad || 0), 0);
+  }
 
-  // obtenerDescuentoPorCantidadFolleto(cantidad: number): number {
-  //   if (cantidad < 10) {
-  //     return 0; // 0% de descuento
-  //   } else if (cantidad > 9 && cantidad < 20) {
-  //     return 0.40; // 40% de descuento
-  //   } else if (cantidad > 19 && cantidad < 50) {
-  //     return 0.45; // 45% de descuento
-  //   } else if (cantidad > 49 && cantidad < 100) {
-  //     return 0.50; // 50% de descuento
-  //   } else {
-  //     return 0.55; // 55% de descuento
-  //   }
-  // }
+  obtenerDescuentoPorCantidadFolleto(cantidad: number): number {
+    if (cantidad < 10) {
+      return 0; // 0% de descuento
+    } else if (cantidad > 9 && cantidad < 20) {
+      return 0.40; // 40% de descuento
+    } else if (cantidad > 19 && cantidad < 50) {
+      return 0.45; // 45% de descuento
+    } else if (cantidad > 49 && cantidad < 100) {
+      return 0.50; // 50% de descuento
+    } else {
+      return 0.55; // 55% de descuento
+    }
+  }
 
-  // calcularPrecioSinDescuentoFolleto(): number {
-  //   return this.mazodoble.reduce((acc, producto) => acc + (producto.cantidad || 0) * producto.precioDolar, 0);
-  // }
+  calcularPrecioSinDescuentoFolleto(): number {
+    return this.slotDeTorneo.reduce((acc, producto) => acc + (producto.cantidad || 0) * producto.precioDolar, 0);
+  }
 
-  // calcularPrecioTotalFolleto(): number {
-  //   const totalUnidades = this.calcularTotalUnidadesFolleto();
-  //   const precioSinDescuento = this.mazodoble.reduce((acc, producto) => acc + (producto.cantidad || 0) * producto.precioDolar, 0);
+  calcularPrecioTotalFolleto(): number {
+    const totalUnidades = this.calcularTotalUnidadesFolleto();
+    const precioSinDescuento = this.slotDeTorneo.reduce((acc, producto) => acc + (producto.cantidad || 0) * producto.precioDolar, 0);
 
-  //   const descuento = this.obtenerDescuentoPorCantidadFolleto(totalUnidades);
-  //   const precioConDescuento = precioSinDescuento * (1 - descuento);
+    const descuento = this.obtenerDescuentoPorCantidadFolleto(totalUnidades);
+    const precioConDescuento = precioSinDescuento * (1 - descuento);
 
-  //   return precioConDescuento;
-  // }
+    return precioConDescuento;
+  }
 
-  // obtenerTextoDescuentoFolleto(): string {
-  //   const totalUnidades = this.calcularTotalUnidadesFolleto();
-  //   const descuento = this.obtenerDescuentoPorCantidadFolleto(totalUnidades);
-  //   const descuentoPorcentaje = Math.round(descuento * 100);
-  //   return descuento > 0 ? `${descuentoPorcentaje}% de descuento` : '';
-  // }
+  obtenerTextoDescuentoFolleto(): string {
+    const totalUnidades = this.calcularTotalUnidadesFolleto();
+    const descuento = this.obtenerDescuentoPorCantidadFolleto(totalUnidades);
+    const descuentoPorcentaje = Math.round(descuento * 100);
+    return descuento > 0 ? `${descuentoPorcentaje}% de descuento` : '';
+  }
 
 
 
