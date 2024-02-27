78e1eacd40cd21d5672f570121970e6858668333
sgrinblat <sgrinblat@asjservicios.com>
2024-02-27 13:13:05 -0300

    por provincia

--- src/app/pages/tiendas/tiendas.component.ts
+++ src/app/pages/tiendas/tiendas.component.ts
@@ -20,9 +20,14 @@ SwiperCore.use([Pagination, Navigation]);
 })
 export class TiendasComponent {
 
-  tiendas!: Tienda[];
-  tiendasPorPais!: Tienda[];
-  tiendasSeleccionadas!: Tienda[];
+  // tiendas!: Tienda[];
+  // tiendasPorPais!: Tienda[];
+  // tiendasSeleccionadas!: Tienda[];
+
+  tiendasSeleccionadas: Tienda[]; // Asumiendo que Tienda es el tipo de tus objetos de tienda
+  tiendasPorPaisSeleccionado: Tienda[] = [];
+  tiendasPorPais: {[pais: string]: Tienda[]} = {}; // Para guardar las tiendas por país
+  provinciasSeleccionadas: {[provincia: string]: boolean} = {};
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object, private conexion: ConexionService) { }
 
@@ -35,10 +40,68 @@ export class TiendasComponent {
   }
 
   obtenerTiendas() {
-    this.conexion.getTodasLasTiendas().subscribe((datos) => {
-      this.tiendasPorPais = datos.flat();
+    this.conexion.getTodasLasTiendas().subscribe((datos: Tienda[]) => {
+      // Llenar `tiendasPorPais` con las tiendas
+      datos.forEach((tienda) => {
+        if (!this.tiendasPorPais[tienda.paisTienda]) {
+          this.tiendasPorPais[tienda.paisTienda] = [];
+        }
+        this.tiendasPorPais[tienda.paisTienda].push(tienda);
+      });
     });
   }
+
+  mostrarTiendasPorPais(pais: string) {
+    // Hacer una copia profunda para trabajar con los datos sin modificar el original
+    this.tiendasPorPaisSeleccionado = [...this.tiendasPorPais[pais]];
+    this.tiendasSeleccionadas = [...this.tiendasPorPaisSeleccionado];
+    this.actualizarProvinciasSeleccionadas();
+  }
+
+  // Asegúrate de usar `tiendasPorPaisSeleccionado` como referencia para filtrar
+  filtrarTiendasPorProvincia() {
+    // Verificar si al menos una provincia está seleccionada
+    const algunaProvinciaSeleccionada = Object.values(this.provinciasSeleccionadas).some(seleccionada => seleccionada);
+
+    if (algunaProvinciaSeleccionada) {
+      // Si alguna provincia está seleccionada, filtrar normalmente
+      this.tiendasSeleccionadas = this.tiendasPorPaisSeleccionado.filter(tienda =>
+        this.provinciasSeleccionadas[tienda.provinciaTienda]
+      );
+    } else {
+      // Si ninguna provincia está seleccionada, mostrar todas las tiendas
+      this.tiendasSeleccionadas = [...this.tiendasPorPaisSeleccionado];
+    }
+  }
+
+
+  actualizarProvinciasSeleccionadas() {
+    // Inicializa las provincias seleccionadas basado en `tiendasPorPaisSeleccionado`
+    this.provinciasSeleccionadas = {};
+    this.tiendasPorPaisSeleccionado.forEach(tienda => {
+      this.provinciasSeleccionadas[tienda.provinciaTienda] = true;
+    });
+    // No es necesario llamar a `filtrarTiendasPorProvincia` aquí si quieres que inicialmente se muestren todas las tiendas
+  }
+
+  onProvinciaCheckboxChange(provincia: string, event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const isChecked = inputElement.checked;
+    this.provinciasSeleccionadas[provincia] = isChecked;
+    this.filtrarTiendasPorProvincia();
+  }
+
+  getProvincias(): string[] {
+    // Usa `tiendasPorPaisSeleccionado` para obtener la lista de provincias
+    const provincias = this.tiendasPorPaisSeleccionado.map(tienda => tienda.provinciaTienda);
+    return [...new Set(provincias)].sort(); // Elimina duplicados
+  }
+
+  // getProvincias(): string[] {
+  //   const provincias = this.tiendasSeleccionadas.map(tienda => tienda.provinciaTienda);
+  //   return [...new Set(provincias)].sort(); // Agregamos .sort() para ordenar alfabéticamente
+  // }
+
 
   mostrarDatosTienda(tienda: Tienda) {
     Swal.fire({
@@ -60,13 +123,6 @@ export class TiendasComponent {
       }
     })
   }
-
-  mostrarTiendasPorPais(pais: string) {
-    this.tiendasSeleccionadas = this.tiendasPorPais.filter(tienda => tienda.paisTienda === pais);
-  }
-
-
-
 
   onImageLoad(event: Event) {
     const imageElement = event.target as HTMLImageElement;
